e8384e67ed5aa9465e74210b4d09cd7b85812852
BootCampBL <satyabhandari43@gmail.com>
2021-07-08 22:20:42 +0530

    [sathya] Add- Program of Swap 2 numbers

---
+++ UC7_Swap2Numbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCorePrograms
+{
+    class UC7_Swap2Numbers
+    {
+        public int NumberOne;
+        public int NumberTwo;
+        public int temp = 0;
+        public void SwapTwoNumber()
+        {
+
+            Console.Write("\nInput the First Number : "); //Input from user
+            NumberOne = Convert.ToInt32(Console.ReadLine());//Read 1st Number
+            Console.Write("\nInput the Second Number : "); //Input from User
+            NumberTwo = Convert.ToInt32(Console.ReadLine());//Read @nd Number
+            temp = NumberOne; // ( storing numTwo in temp )
+            NumberOne = NumberTwo; //(in numOne Storing NumberTwo)
+            NumberTwo = temp; //(storing Temp value in Number 2)
+            Console.Write("\nAfter Swapping : "); //for show
+            Console.Write("\nFirst Number : " + NumberOne); //Op After swaping
+            Console.Write("\nSecond Number : " + NumberTwo);//Op after swaping
+            Console.Read();
+        }
+    }
+}
+   
